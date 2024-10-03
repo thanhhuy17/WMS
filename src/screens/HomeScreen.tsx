8963a36796426bcc905765b34bb10a100c85fb66
Nguyen Thanh Huy <huymau170819999@gmail.com>
2024-10-03 22:09:30 +0700

    check

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,14 @@
-import { Button } from 'antd'
-import React from 'react'
+import { Button } from "antd";
+import React from "react";
 
 const HomeScreen = () => {
   return (
     <div>
-        {/* <button className='btn btn-xl btn-danger'> Enter</button> */}
+      {/* <button className='btn btn-xl btn-danger'> Enter</button> */}
 
-        <Button>hello</Button>
+      <Button>hello o</Button>
     </div>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
