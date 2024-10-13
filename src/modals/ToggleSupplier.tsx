f76ef712b39d84cd3db52901e1aea1700e4867f0
Nguyen Thanh Huy <huymau170819999@gmail.com>
2024-10-14 00:03:07 +0700

    update Form ToggleSupplier

--- src/modals/ToggleSupplier.tsx
+++ src/modals/ToggleSupplier.tsx
@@ -1,6 +1,7 @@
-import { Form, message, Modal } from "antd";
-import React, { useState } from "react";
+import { Button, Form, Input, message, Modal, Select } from "antd";
+import { useState } from "react";
 import handleAPI from "../apis/handleAPI";
+import { colors } from "../constants/colors";
 
 interface Props {
   visible: boolean;
@@ -12,7 +13,10 @@ interface Props {
 const ToggleSupplier = (props: Props) => {
   const { visible, onClose, onAddNew, supplier } = props;
   const [isLoading, setIsLoading] = useState(false);
+  const [isTaking, setIsTaking] = useState<boolean>();
   const [form] = Form.useForm();
+
+  // ---------- SEND DATA TO BACKEND ------------
   const addNewSupplier = async (values: Props) => {
     setIsLoading(true);
     try {
@@ -25,14 +29,110 @@ const ToggleSupplier = (props: Props) => {
       // dispatch to redux
     } catch (error) {}
   };
-
+  const handleClose = () => {
+    form.resetFields();
+    onClose();
+  };
   return (
     <div>
       <Modal
-        title="Add Supplier"
+        // width={720}
+        open={visible}
+        onClose={handleClose}
+        onCancel={handleClose}
+        // title="Add Supplier"
+        title={
+          <span style={{ color: `${colors.mainColor}` }}>Add Supplier</span>
+        }
         okText="Add Supplier"
         cancelText="Discard"
-      ></Modal>
+      >
+        <Form
+          onFinish={addNewSupplier}
+          layout="horizontal"
+          labelCol={{ span: 6 }}
+          wrapperCol={{ span: 18 }}
+          size="middle"
+          form={form}
+          style={{ color: `${colors.mainColor}` }}
+        >
+          <Form.Item
+            name={"name"}
+            rules={[
+              {
+                required: true,
+                message: "Please Enter Supplier Name!",
+              },
+            ]}
+            label={
+              <span style={{ color: `${colors.mainColor}` }}>
+                Supplier Name
+              </span>
+            }
+            colon={false}
+          >
+            <Input placeholder="Enter Supplier Name" allowClear />
+          </Form.Item>
+          <Form.Item
+            name={"product"}
+            label={
+              <span style={{ color: `${colors.mainColor}` }}>Product</span>
+            }
+            colon={false}
+          >
+            <Input placeholder="Enter Product" allowClear />
+          </Form.Item>
+          <Form.Item
+            name={"category"}
+            label={
+              <span style={{ color: `${colors.mainColor}` }}>Category</span>
+            }
+            colon={false}
+          >
+            <Select options={[]} placeholder="Select product category" />
+          </Form.Item>
+          <Form.Item
+            name={"buying-price"}
+            label={
+              <span style={{ color: `${colors.mainColor}` }}>Buying Price</span>
+            }
+            colon={false}
+          >
+            <Input placeholder="Enter buying price" allowClear />
+          </Form.Item>
+          <Form.Item
+            name={"contact-number"}
+            label={
+              <span style={{ color: `${colors.mainColor}` }}>
+                Supplier Name
+              </span>
+            }
+            colon={false}
+          >
+            <Input placeholder="Enter supplier contact number" allowClear />
+          </Form.Item>
+          <Form.Item
+            name={"type"}
+            label={<span style={{ color: `${colors.mainColor}` }}>Type</span>}
+            colon={false}
+          >
+            <div className="mb-2">
+              <Button
+                onClick={() => setIsTaking(false)}
+                type={isTaking === false ? "primary" : "default"}
+              >
+                Not taking return
+              </Button>
+            </div>
+            <Button
+              onClick={() => setIsTaking(true)}
+              type={isTaking ? "primary" : "default"}
+            >
+              Taking return
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
     </div>
   );
 };
--- src/screens/SupplierScreen.tsx
+++ src/screens/SupplierScreen.tsx
@@ -5,8 +5,11 @@ import { colors } from "../constants/colors";
 import { LuFilter } from "react-icons/lu";
 import { PiExportLight } from "react-icons/pi";
 import { MdOutlineAddToPhotos } from "react-icons/md";
+import { useState } from "react";
+import ToggleSupplier from "../modals";
 
 const SupplierScreen = () => {
+  const [isVisibleAddNew, setIsVisibleAddNew] = useState(false);
   const { Title } = Typography;
   const columns: ColumnProps<any>[] = [];
   return (
@@ -17,17 +20,33 @@ const SupplierScreen = () => {
         title={() => (
           <div className="row">
             <div className="col">
-              <Title  style={{fontSize: '20px', color:`${colors.mainColor}`}}>Suppliers</Title>
+              <Title style={{ fontSize: "20px", color: `${colors.mainColor}` }}>
+                Suppliers
+              </Title>
             </div>
             <div className="col text-end">
               <Space>
-                <Button icon={<MdOutlineAddToPhotos size={20}/>} type="primary">Add Supplier</Button>
-                <Button icon = {<LuFilter size={20}/>}>Filters</Button>
-                <Button icon={<PiExportLight size={20}/>}>Export</Button>
+                <Button
+                  icon={<MdOutlineAddToPhotos size={20} />}
+                  type="primary"
+                  // Show Modal Supplier when to onclick
+                  onClick={() => setIsVisibleAddNew(true)}
+                >
+                  Add Supplier
+                </Button>
+                <Button icon={<LuFilter size={20} />}>Filters</Button>
+                <Button icon={<PiExportLight size={20} />}>Export</Button>
               </Space>
             </div>
           </div>
         )}
+      />
+      <ToggleSupplier
+        visible={isVisibleAddNew}
+        onClose={() => setIsVisibleAddNew(false)}
+        onAddNew={(val) => {
+          console.log(val);
+        }}
       />
     </div>
   );
