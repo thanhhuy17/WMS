8a372099dff12f3f812944e71cd2ecd3048448b1
HuyNguyen <huymau170819999@gmail.com>
2024-12-27 23:43:45 +0700

    oke

--- src/utils/replaceName.ts
+++ src/utils/replaceName.ts
@@ -10,10 +10,10 @@
 // }
 
 export const replaceName = (str: string) => {
-    // if (!str || typeof str !== 'string') {
-    //     console.error('Invalid input to replaceName:', str);
-    //     throw new TypeError('Input must be a valid string');
-    // }
+    if (!str || typeof str !== 'string') {
+        console.error('Invalid input to replaceName:', str);
+        throw new TypeError('Input must be a valid string');
+    }
     return str
         .normalize('NFD')
         .toLocaleLowerCase()
