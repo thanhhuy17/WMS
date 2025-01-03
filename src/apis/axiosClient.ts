a85ce0f0ef0ab7738304e0086f4e5aaa792cb85f
Nguyen Thanh Huy <huymau170819999@gmail.com>
2025-01-03 22:56:26 +0700

    commit

--- src/apis/axiosClient.ts
+++ src/apis/axiosClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import queryString from "query-string";
 import { localDataNames } from "../constants/appInfos";
 
-const baseURL = `http://192.168.100.194:3001` // ip của wifi HuyNguyen
+const baseURL = `http://192.168.100.194:3001` // ip của wifi HuyNguyen Change 03/01/2024
 // const baseURL = `http://192.168.2.125:3002`
 
 const getAccessToken = () => {
