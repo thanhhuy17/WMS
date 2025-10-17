1e4c4ded5ee58e796428da1e9b5c222801e1b8e8
thanhhuy17 <huymau170819999@gmail.com>
2025-10-17 22:05:45 +0700

    newID for Mac

--- src/apis/axiosClient.ts
+++ src/apis/axiosClient.ts
@@ -3,7 +3,7 @@ import queryString from "query-string";
 import { localDataNames } from "../constants/appInfos";
 
 //const baseURL = `http://192.168.100.194:3001` // ip của wifi HuyNguyen Change 03/01/2024
-const baseURL = `http://192.168.100.151:3002` // Máy Dell HuyBen99 Mua 3tr 
+const baseURL = `http://192.168.100.190:3001` // Máy MAC
 // const baseURL = `http://192.168.2.125:3002`
 
 const getAccessToken = () => {
